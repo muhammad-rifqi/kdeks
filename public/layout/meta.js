52615c73d0f245824c7019ab117688248fb7c9f6
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-09-05 21:57:07 +0700

    update

---
+++ public/layout/meta.js
@@ -0,0 +1,48 @@
+document.write(`
+<meta charset="utf-8" />
+  <meta name="viewport"
+    content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+
+  <title>KDEKS - Sejarah</title>
+
+  <meta name="description" content="" />
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="/assets/img/favicon/favicon.ico" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap"
+    rel="stylesheet" />
+
+  <!-- Icons -->
+  <link rel="stylesheet" href="/assets/vendor/fonts/remixicon/remixicon.css" />
+  <link rel="stylesheet" href="/assets/vendor/fonts/flag-icons.css" />
+
+  <!-- Menu waves for no-customizer fix -->
+  <link rel="stylesheet" href="/assets/vendor/libs/node-waves/node-waves.css" />
+
+  <!-- Core CSS -->
+  <link rel="stylesheet" href="/assets/vendor/css/rtl/core.css" />
+  <link rel="stylesheet" href="/assets/vendor/css/rtl/theme-default.css" />
+
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+  <link rel="stylesheet" href="/assets/vendor/libs/typeahead-js/typeahead.css" />
+  <link rel="stylesheet" href="/assets/vendor/libs/flatpickr/flatpickr.css" />
+  <!-- Row Group CSS -->
+  <link rel="stylesheet" href="/assets/vendor/libs/datatables-rowgroup-bs5/rowgroup.bootstrap5.css" />
+  <!-- Form Validation -->
+  <link rel="stylesheet" href="/assets/vendor/libs/@form-validation/form-validation.css" />
+
+   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+  <!-- Page CSS -->
+
+  <!-- Helpers -->
+  <script src="/assets/vendor/js/helpers.js"></script>
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="/assets/js/config.js"></script>
+  `);
