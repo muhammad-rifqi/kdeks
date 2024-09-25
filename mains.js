ba018cf45d97aa473c1769e5e59820be1debfaff
Muhammad Rifqi <ats.muhammadr@xl.co.id>
2024-09-25 15:20:54 +0700

    update

--- mains.js
+++ mains.js
@@ -27,7 +27,11 @@ apps.get('/dashboard', (req, res) => {
 })
 
 apps.get('/news', (req, res) => {
-    res.sendFile(path.resolve('./views/news.html'));
+    res.sendFile(path.resolve('./views/news_management/news.html'));
+})
+
+apps.get('/news_category', (req, res) => {
+    res.sendFile(path.resolve('./views/news_management/news_category.html'));
 })
 
 apps.get('/tentangkami', (req, res) => {
@@ -44,6 +48,30 @@ apps.get('/sk', (req, res) => {
 
 apps.get('/video', (req, res) => {
     res.sendFile(path.resolve('./views/profile/video.html'));
+})
+
+apps.get('/photo', (req, res) => {
+    res.sendFile(path.resolve('./views/profile/photos.html'));
+})
+
+apps.get('/agenda', (req, res) => {
+    res.sendFile(path.resolve('./views/agenda/agenda.html'));
+})
+
+apps.get('/home_management', (req, res) => {
+    res.sendFile(path.resolve('./views/home_management/home.html'));
+})
+
+apps.get('/elibrary', (req, res) => {
+    res.sendFile(path.resolve('./views/e-library/elibrary.html'));
+})
+
+apps.get('/users', (req, res) => {
+    res.sendFile(path.resolve('./views/user_management/users.html'));
+})
+
+apps.get('/opini', (req, res) => {
+    res.sendFile(path.resolve('./views/opini/opini.html'));
 })
 
 apps.listen(3002);
