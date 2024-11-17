14e67eac57f84fe3225432195d4f1e9762ad8706
rifqisolutif <muhammad.rifqi@solutif.co.id>
2024-11-17 19:41:22 +0700

    update

--- db/config.js
+++ db/config.js
@@ -1,41 +1,32 @@
 const util = require('util');
-// const mysql = require('mysql2');
-// const pool = mysql.createPool({
-//     connectionLimit: 10,
-//     host: "localhost",
-//     user: "root",
-//     password: "S#g=qGHo7i<t5",
-//     database: "kdeks"
-// });
-
-const pgsql = require('pg');
-const pool = new pgsql.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'kdeks',
-    password: '123',
-    port: 5432,
-})
+const mysql = require('mysql2');
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "S#g=qGHo7i<t5",
+    database: "kdeks"
+});
 
 // password: "S#g=qGHo7i<t5",
 
-// pool.getConnection((err, connection) => {
-//     if (err) {
-//         if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-//             console.error('Database connection was closed.');
-//         }
-//         if (err.code === 'ER_CON_COUNT_ERROR') {
-//             console.error('Database has too many connections.');
-//         }
-//         if (err.code === 'ECONNREFUSED') {
-//             console.error('Database connection was refused.');
-//         }
-//     }
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.');
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.');
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.');
+        }
+    }
 
-//     if (connection) connection.release();
+    if (connection) connection.release();
 
-//     return;
-// });
+    return;
+});
 
 pool.query = util.promisify(pool.query);
 
