1611bed3116fb288a8e3e534bdabe908ce3c4e22
rifqisolutif <muhammad.rifqi@solutif.co.id>
2024-11-22 15:35:19 +0700

    update

--- db/query.js
+++ db/query.js
@@ -736,7 +736,7 @@ const insertfileupload = async (req, res) => {
     const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
     const timeupdate = date + ' ' + time;
     const file_date = req.body.date;
-    const fileuploads = req.file.originalname.replace(" ", "");
+    const fileuploads = "https://kdeks.rifhandi.com/uploads/news/" + req.file.originalname.replace(" ", "");
     const sql = await executeQuery("insert into reports(title,title_en,content,content_en,file,is_publish,date,created_at,updated_at,report_category_id,web_identity) values(?,?,?,?,?,?,?,?,?,?,?)",
         [req.body.title, req.body.title_en, req.body.content, req.body.content_en, fileuploads, req.body.is_publish, file_date, timeupdate, timeupdate, req.body.file_category_id, 'kdeks']);
     if (sql) {
@@ -765,7 +765,7 @@ const updatefileupload = async (req, res) => {
             res.redirect('/elibrary');
         }
     } else {
-        const fileuploads = req.file.originalname.replace(" ", "");
+        const fileuploads = "https://kdeks.rifhandi.com/uploads/news/" + req.file.originalname.replace(" ", "");
         const sql = await executeQuery("update reports set title=?,title_en=?,content=?,content_en=?,file=?,is_publish=?,date=?,created_at=?,updated_at=?,report_category_id=? where id = ?",
             [req.body.title, req.body.title_en, req.body.content, req.body.content_en, fileuploads, req.body.is_publish, file_date, timeupdate, timeupdate, req.body.file_category_id, req.body.id]);
         if (sql) {
